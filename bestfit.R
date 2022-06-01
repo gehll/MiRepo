59688780131ad39fae8fdf84f9979a5fe926c18e
Gonzalo <gehllanza@gmail.com>
2022-06-01 13:01:55 +0200

    Rama2 modifies bestfit.R

--- bestfit.R
+++ bestfit.R
@@ -1,7 +1,8 @@
 # Este es un archivo que se añadirá a MAIN
 # Esta función recibe a la otra y saca un modelo con el mejor grado
 
-bestfit <- function(bestdegree, datos, regresor, respuesta){
-  fit <- glm(respuesta ~ poly(regresor, bestdegree), data = datos)
+bestfit <- function(degree, datos, regresor, respuesta){
+  
+  fit <- glm(respuesta ~ poly(regresor, degree), data = datos)
   return(fit)
 }
