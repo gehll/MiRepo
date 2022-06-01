743102ad2a05ffe6a2535f2e46de4316b6e5a05c
Gonzalo <gehllanza@gmail.com>
2022-06-01 17:30:47 +0200

    generar conflictos

--- bestfit.R
+++ bestfit.R
@@ -1,12 +1,14 @@
-# Este es un archivo que se añadirá a MAIN
+# Este es un archivo que se añadirá a MAIN y que ha modificadoi Gonzalo1
+#Debería haber conflictos a la hora de hacer el pull request
 
 # Esta función fhfffg a la otra y saca un modelo con el mejor grado
 
 
 bestfit <- function(degree, datos, regresor, respuesta){
-  modelo <- glm(respuesta ~ poly(regresor, degree), data = datos)
-  return(modelo)
-
-  }
+  fit <- glm(respuesta ~ poly(regresor, degree), data = datos)
+  
+  return(fit)
+  
+}
 
 # He añadido esta línea para hacer un push a remoto y después un PULL REQUEST para hacer un merge a MAIN
